c4700230d672e592bc7d5107a82502e3f5e6b910
Senén Rodero Rodríguez <senenrodero@gmail.com>
2020-11-23 17:00:17 +0100

    Add SDG LocalTarget model

--- spec/factories/sdg.rb
+++ spec/factories/sdg.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
   factory :sdg_target, class: "SDG::Target" do
     sequence(:code, 1) { |n| "#{n}.#{n}" }
   end
+
+  factory :sdg_local_target, class: "SDG::LocalTarget" do
+    sequence(:code, 1)        { |n| "#{n}.#{n}.#{n}" }
+    sequence(:title, 1)       { |n| "Local Target #{n} title" }
+    sequence(:description, 1) { |n| "Help for Local Target #{n}" }
+
+    target { SDG::Target[code.rpartition(".").first] }
+  end
 end
